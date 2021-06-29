09d04f77be59f865106109b59bc5ff0106eaf18b
Antonioflo13 <floreantonio@hotmail.it>
2021-06-29 15:37:36 +0200

    header

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://img.icons8.com/color/452/google-logo.png">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Domande frequenti - Privacy e termini - Google</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <!-- header -->
+    <header>
+
+        <!-- nav -->
+        <nav class="wrapper">
+
+            <!-- nav-top -->
+            <div class="nav-top">
+                <div class="nav-left">
+                    <img src="https://pics.freeicons.io/uploads/icons/png/13946100361580378262-512.png" alt="logo">
+                    <h3>Privacy e termini</h3>
+                </div>
+
+                <div class="nav-right">
+                    <i class="fas fa-th"></i>
+                    <img src="https://thumbs.dreamstime.com/b/immagine-di-profilo-dell-avatar-del-bambino-cappello-d-uso-aviatore-ragazzo-con-il-ritratto-personaggio-dei-cartoni-animati-vetro-149138397.jpg" alt="avatar">
+                </div>
+            </div>
+            <!--/ nav-top -->
+
+            <!-- nav-bottom -->
+            <div>
+
+            </div>
+            <!--/ nav-bottom -->
+
+        </nav>
+        <!-- /nav -->
+
+    </header>
+    <!-- /header -->
+
+</body>
+</html>
